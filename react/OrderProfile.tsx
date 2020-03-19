ddb40a00b0e4ccacb34338b3febdf035c391452b
Lucas Cordeiro <ecdb.lucas@gmail.com>
2020-03-19 19:17:54 -0300

    add setClientPreferences data function in context

--- react/OrderProfile.tsx
+++ react/OrderProfile.tsx
@@ -1,16 +1,24 @@
 import React, { useMemo, useContext, useCallback } from 'react'
 import { useMutation } from 'react-apollo'
 import MutationUpdateOrderFormProfile from 'vtex.checkout-resources/MutationUpdateOrderFormProfile'
+import MutationUpdateClientPreferencesData from 'vtex.checkout-resources/MutationUpdateClientPreferencesData'
 import {
   useOrderQueue,
   useQueueStatus,
   QueueStatus,
 } from 'vtex.order-manager/OrderQueue'
 import { useOrderForm } from 'vtex.order-manager/OrderForm'
-import { OrderForm, UserProfileInput } from 'vtex.checkout-graphql'
+import {
+  OrderForm,
+  UserProfileInput,
+  ClientPreferencesDataInput,
+} from 'vtex.checkout-graphql'
 
 interface ProfileContext {
   setOrderProfile: (profile: UserProfileInput) => Promise<{ success: boolean }>
+  setClientPreferencesData: (
+    clientPreferences: ClientPreferencesDataInput
+  ) => Promise<{ success: boolean }>
 }
 
 const OrderProfileContext = React.createContext<ProfileContext | undefined>(
@@ -25,7 +33,16 @@ interface UpdateOrderFormProfileMutationVariables {
   profile: UserProfileInput
 }
 
+interface UpdateClientPreferencesDataMutation {
+  updateClientPreferencesData: OrderForm
+}
+
+interface UpdateClientPreferencesDataMutationVariables {
+  clientPreferences: ClientPreferencesDataInput
+}
+
 const SET_PROFILE_TASK = 'SetProfileTask'
+const SET_CLIENT_PREFERENCES_TASK = 'SetClientPreferencesTask'
 
 export const OrderProfileProvider: React.FC = ({ children }) => {
   const { enqueue, listen } = useOrderQueue()
@@ -37,6 +54,10 @@ export const OrderProfileProvider: React.FC = ({ children }) => {
     UpdateOrderFormProfileMutation,
     UpdateOrderFormProfileMutationVariables
   >(MutationUpdateOrderFormProfile)
+  const [updateClientPreferencesData] = useMutation<
+    UpdateClientPreferencesDataMutation,
+    UpdateClientPreferencesDataMutationVariables
+  >(MutationUpdateClientPreferencesData)
 
   const setOrderProfile = useCallback(
     async (profile: UserProfileInput) => {
@@ -69,7 +90,37 @@ export const OrderProfileProvider: React.FC = ({ children }) => {
     [updateOrderFormProfile, enqueue, queueStatusRef, setOrderForm]
   )
 
-  const value = useMemo(() => ({ setOrderProfile }), [setOrderProfile])
+  const setClientPreferencesData = useCallback(
+    async (clientPreferences: ClientPreferencesDataInput) => {
+      const task = async () => {
+        const { data } = await updateClientPreferencesData({
+          variables: { clientPreferences },
+        })
+
+        const orderForm = data!.updateClientPreferencesData
+
+        return orderForm
+      }
+
+      try {
+        const newOrderForm = await enqueue(task, SET_CLIENT_PREFERENCES_TASK)
+
+        if (queueStatusRef.current === QueueStatus.FULFILLED) {
+          setOrderForm(newOrderForm)
+        }
+
+        return { success: true }
+      } catch (err) {
+        return { success: false }
+      }
+    },
+    [enqueue, queueStatusRef, setOrderForm, updateClientPreferencesData]
+  )
+
+  const value = useMemo(() => ({ setOrderProfile, setClientPreferencesData }), [
+    setOrderProfile,
+    setClientPreferencesData,
+  ])
 
   return (
     <OrderProfileContext.Provider value={value}>
