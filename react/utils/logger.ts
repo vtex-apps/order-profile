cf68a172bee4f5ad788cc86260af6b6ea0381662
Arthur Sampaio <arthur.correia@vtex.com.br>
2021-12-23 09:09:37 -0300

    Fix endless loop

--- react/utils/logger.ts
+++ react/utils/logger.ts
@@ -1,3 +1,5 @@
+import { useCallback } from 'react'
+
 export interface LogParams {
   type: 'Error'
   level: 'Critical'
@@ -11,22 +13,19 @@ export type LogFn = (params: LogParams) => void
 export type UseLogger = () => { log: LogFn }
 
 export function useLogger() {
-  const log = ({
-    type,
-    level,
-    event,
-    workflowType,
-    workflowInstance,
-  }: LogParams) => {
-    // eslint-disable-next-line no-console
-    console.log({
-      type,
-      level,
-      event,
-      workflowInstance,
-      workflowType: workflowType || 'OrderProfile',
-    })
-  }
+  const log = useCallback(
+    ({ type, level, event, workflowType, workflowInstance }: LogParams) => {
+      // eslint-disable-next-line no-console
+      console.log({
+        type,
+        level,
+        event,
+        workflowInstance,
+        workflowType: workflowType || 'OrderProfile',
+      })
+    },
+    []
+  )
 
   return { log }
 }
